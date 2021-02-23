489b312c9bab3703128aae5897711b3313f9db52
Manuel Antonio Cebreros Diaz <a01112128@itesm.mx>
2021-02-22 22:24:16 -0600

    Source code

---
+++ Source/MobileMessage.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+public static class MobileMessage
+{
+#if UNITY_IOS
+
+	[DllImport("__Internal")]
+	private static extern void _ShowAlert(string title, string message);
+#endif
+
+	public static void ShowAlert(string title,string message)
+	{
+#if UNITY_EDITOR
+		Debug.Log(title + ": " + message);
+#elif UNITY_IOS
+		_ShowAlert(title,message);
+#elif UNITY_ANDROID
+		Debug.Log(title + ": " + message);
+		AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+		object[] toastParams = new object[3];
+		AndroidJavaClass unityActivity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		toastParams[0] = unityActivity.GetStatic<AndroidJavaObject>("currentActivity");
+		toastParams[1] = title + ": " + message;
+		toastParams[2] = toastClass.GetStatic<int>("LENGTH_LONG");
+		AndroidJavaObject toastObject =	toastClass.CallStatic<AndroidJavaObject>("makeText", toastParams);
+		toastObject.Call("show");
+#endif
+	}
+}
